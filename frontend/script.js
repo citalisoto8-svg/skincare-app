cd4cb565c5c0e712778a5eb38afa855bdcb704e2
Citlali Soto <citalisoto8@gmail.com>
2026-03-01 16:15:15 -0600

    Actualización del frontend: index, register, login, dashboard y CSS

---
+++ frontend/script.js
@@ -0,0 +1,26 @@
+// app.js
+const registerForm = document.getElementById('registerForm');
+
+if (registerForm) {
+  registerForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    try {
+      const res = await fetch('http://localhost:3000/auth/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, email, password })
+      });
+
+      const data = await res.json();
+      alert(data.message);
+    } catch (err) {
+      console.error(err);
+      alert('Error al registrarse');
+    }
+  });
+}
